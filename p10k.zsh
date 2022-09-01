4f19df2545549cbfe653a7edfa31a1f55a43d48f
Arpit Agarwal <15676216+arpitjindal97@users.noreply.github.com>
2022-09-01 23:50:39 +0200

    Change prompt background

--- p10k.zsh
+++ p10k.zsh
@@ -152,7 +152,7 @@
   fi
 
   # Default background color.
-  typeset -g POWERLEVEL9K_BACKGROUND=234
+  typeset -g POWERLEVEL9K_BACKGROUND=235
 
   # Separator between same-color segments on the left.
   typeset -g POWERLEVEL9K_LEFT_SUBSEGMENT_SEPARATOR='%242F\uE0B1'
